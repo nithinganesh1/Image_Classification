44deda124cbbb8370d2ffcd1e07b156bf32adf3a
Nithin G <nithingganesh1@gmail.com>
2024-02-27 10:18:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,11 +96,12 @@ def main():
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image.", use_column_width=400)
-    else:
-        st.write("#### !Please check emotions only after uploading an image.")
+
     submit=st.button("Check Emotion")
 
-    if submit:
+    if submit and uploaded_file is None:
+        st.error("! Please upload an image before proceeding.")
+    else:
         final_data = input_image_setup(uploaded_file)
         prediction = predict_img(final_data)
         st.subheader("The emotion is")
