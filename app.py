fcddf8c31e6a1cee7127301828d6248647665328
Nithin G <nithingganesh1@gmail.com>
2024-02-27 10:36:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def main():
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image.", use_column_width=400)
+        st.image(image, caption="Uploaded Image.", width = 400)
 
     submit=st.button("Check Emotion")
 
